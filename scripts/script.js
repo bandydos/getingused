84b4aafbe2b08844d010e4ea7f7b1486ad30d128
bandydos <alexandertack@hotmail.be>
2020-02-12 18:26:44 +0100

    Another commit, bang

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,4 @@ var btnClick = document.getElementById("btnClick");
 btnClick.onclick = function(){
     var divs = document.getElementsByName("div");
 }
-//Just a comment so that I can commit something 
+//Just a comment so that I can commit something for the second time :)
